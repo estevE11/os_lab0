9b02392b2c424a5de2724fd135e98b25f64d2367
Roger Esteve <rogeresteve30@gmail.com>
2023-01-11 12:44:31 +0100

    ara si el que demanaba la practica

--- main.c
+++ main.c
@@ -5,6 +5,8 @@
 int random(int max);
 int random_from_seed(int seed, int max);
 long long current_timestamp();
+int compared(int a, int b);
+int getcenter(int a, int b);
 
 int main(int argc, char *argv[]) {
     int max = 100;
@@ -13,31 +15,49 @@ int main(int argc, char *argv[]) {
     }
 
     printf("Generating a number from 1 to %d\n\n", max);
-    int n = random(max)+1;
+    int rand = random(max)+1;
 
-    int x;
+    int x = max/2;
+    int lo = 0, hi = max;
+
+    int found = 0;
 
     for (int i = 0; i < 10; i++) {
-        printf("Attempts left: %d\n", 10-i);
-        printf("Try to guess the number: \n");
-        scanf("%d", &x);
-        if (x > n) {
-            printf("The correct number is LOWERn\n");
+        printf("Current number: %d\n", x, lo, hi);
+        if (compared(rand, x) < 0) {
+            printf("The correct number is LOWER\n\n");
+            hi = x;
+            x = getcenter(lo, x);
+
         }
-        else if (x < n) {
+        else if (compared(rand, x) > 0) {
             printf("The correct number is HIGHER\n\n");
-        }
-        else {
+            lo = x;
+            x = getcenter(x, hi);
+        } else {
             printf("The number is CORRECT!\n");
+            found = 1;
             break;
         }
     }
+    if(found == 0) printf("\n\nCorrect number not found!! (%d)", rand);
     return 0;
+}
+
+// returns 0 if a == b
+// returns positive number if a > b
+// returns negative number if a < b
+int compared(int a, int b) {
+    return a - b;
+}
+
+int getcenter(int a, int b) {
+    return ((b - a) / 2 + a);
 }
 
 // Problem:
 // If we try generating random numbers too fast
-// the timestaps doesn't change so the seed is
+// the timestap does not change so the seed is
 // the same.
 int random(int max) {
     long long t = current_timestamp();
@@ -55,7 +75,7 @@ int random_from_seed(int seed, int max) {
 long long current_timestamp()
 {
     struct timeval te;
-    gettimeofday(&te, NULL);                                         // get current time
+    gettimeofday(&te, NULL); // get current time
     long long milliseconds = te.tv_sec * 1000LL + te.tv_usec / 1000; // calculate milliseconds
     // printf("milliseconds: %lld\n", milliseconds);
     return milliseconds;
